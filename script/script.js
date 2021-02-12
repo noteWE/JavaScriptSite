dadaedf36c056ea30f4e7ba293f811f71e0f0137
Ilya Pechkin <ilyapech01@gmail.com>
2021-02-12 23:55:08 +0400

    Added menu

--- script/script.js
+++ script/script.js
@@ -1,4 +1,9 @@
 function initPage() {
+    if (localStorage.getItem("userName") != null) {
+        console.log("login saccessful");
+        document.getElementById("registration").style.display = "none";
+        document.getElementById("menu-username").innerText = localStorage.getItem("userName");
+    }
     let buttonRegistation = document.getElementById("registration-form-button");
     buttonRegistation.onclick = clickButtonRegistration;
 }
@@ -6,6 +11,8 @@ function initPage() {
 function clickButtonRegistration() {
     let inputName = document.getElementById("registration-form-field-name");
     localStorage.setItem("userName", inputName.value);
+    document.getElementById("registration").style.display = "none";
+    document.getElementById("menu-username").innerText = localStorage.getItem("userName");
 }
 
 initPage();
